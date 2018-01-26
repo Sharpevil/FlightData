e99af2b48ca7185658a82bea5b994b2700810c10
Sharpevil <Sharpevil@gmail.com>
2018-01-25 19:33:15 -0500

    Added first draft of on_path function, included test.

--- FlightChecker.py
+++ FlightChecker.py
@@ -10,10 +10,21 @@ def create_flight(flight):
     return Flight(split[0], split[1], split[2], split[3], split[4], split[5])
 
 
-#Check to see if a flight is on a given path, within leniency units of distance
+#Check to see if a flight is on a given path, within leniency km of distance
 def on_path(flight, path, leniency):
+    path_point_a = frame.GeoPoint(float(path.wpt1_wgs_dlat), float(path.wpt1_wgs_dlong), degrees=True)
+    path_point_b = frame.GeoPoint(float(path.wpt2_wgs_dlat), float(path.wpt2_wgs_dlong), degrees=True)
+    flight_point = frame.GeoPoint(float(flight.lat), float(flight.long), degrees=True)
+    geo_path = nv.GeoPath(path_point_a, path_point_b)
 
-    return
+    xt_dist = abs(geo_path.cross_track_distance(flight_point, method='greatcircle').ravel())
+    global smallest_dist
+    if xt_dist < smallest_dist:
+        global closest_path
+        closest_path = path
+        smallest_dist = xt_dist
+
+    return xt_dist < leniency
 
 
 #Check to see if a flight's heading matches that of a path, within leniency degrees
@@ -42,3 +53,19 @@ def cross_track_test():
     pointC = pathA.closest_point_on_great_circle(pointB)
     print pathA.on_path(pointC)
     return
+
+import DataImport
+
+paths = DataImport.import_paths("C:\Users\\armstr\Google Drive\ASRC\DAFIF\Airways\ATS.TXT")
+flights = DataImport.import_flights("C:\Users\\armstr\Google Drive\ASRC\DAFIF\August8WriteFile.csv")
+
+for i in xrange(10):
+    global smallest_dist
+    global closest_path
+    smallest_dist = 100000
+    closest_path = None
+    print "Beginning flight " + str(i)
+    for path in paths:
+        if on_path(flights[i], path, 15):
+            print "Flight " + str(i) + " is on path " + str(path.ats_ident)
+    print "The closest path is " + str(closest_path.ats_ident) + " at " + str(smallest_dist) + "km"
