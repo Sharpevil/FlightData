752250d313c508f73de219455e33b5072bfe3ace
00Bren00 <ringelb8@students.rowan.edu>
2018-02-09 12:36:21 -0500

    Update flightpreprocess.py
    prettied up the code

--- LiveFlights/flightpreprocess.py
+++ LiveFlights/flightpreprocess.py
@@ -1,80 +1,6 @@
-from sklearn import tree
-
-num_of_test_flights = 5533
-num_of_flights = 110646-5533
-
-
-def preprocess(flights_path = "C:\\Users\\remem\\Downloads\\flights\\training.csv", test_flights_path = "C:\\Users\\remem\\Downloads\\flights\\tests.csv"):
-    # df = pd.read_csv(flights_file, usecols=[1, 2, 3, 9])
-    # print(type(df))
-    # print(type (df.))
-    # print(type(df[0][0]))
-
-    alldata = [None]*num_of_flights
-    alllabels = [None]*num_of_flights
-    lineNum = 0
-    flights_file = open(flights_path)
-    for line in flights_file:
-        if(lineNum%1000 == 0):
-            print(lineNum)
-
-        lineList = line.split(',')#do stuff
-
-        #messageTime = lineList[0]
-        if(len(lineList) > 1):
-            latitude = (lineList[1])
-            longitude = (lineList[2])
-            altitude = (lineList[3])
-            heading = (lineList[9])
-            #airCraftID = lineList[7]
-            callSign = lineList[8]
-            #ingestionTime = lineList[13]
-
-            data = [float(latitude) , float(longitude), float(altitude), float(heading)]
-            label = callSign
-            alldata[lineNum] = data
-            alllabels[lineNum ] = label
-            lineNum = lineNum + 1
-
-        if (lineNum == 110646):  # 110646): #1047266*/):
-            break
-    # print( len(alldata))
-    # print(len(alllabels))
-    # print(alllabels)
-    flights_file.close()
-    clf = tree.DecisionTreeClassifier()
-    clf.fit(alldata,
-            alllabels
-            )
-
-    test_flights_file = open(test_flights_path)
-    alltestdata = [None]*num_of_test_flights
-    alltestlabels = [None]*num_of_test_flights
-    lineNum = 0
-
-    for line in test_flights_file:
-        lineList = line.split(',')#do stuff
-        latitude = (lineList[1])
-        longitude = (lineList[2])
-        altitude = (lineList[3])
-        heading = (lineList[9])
-        # airCraftID = lineList[7]
-        callSign = lineList[8]
-        # ingestionTime = lineList[13]
-
-        data = [float(latitude), float(longitude), float(altitude), float(heading)]
-        label = callSign
-        alltestdata[lineNum] = data
-        alltestlabels[lineNum] = label
-        lineNum = lineNum + 1
-    
-    test_flights_file.close()
-    print(clf.score(alltestdata, alltestlabels))
-    print(clf.predict([[39.20906,-71.62375,38000,298.0]]))
-
 # preprocess()
 
-def setUpFiles(flights_path = "C:\\Users\\remem\\Downloads\\flights\\sorted_2018-01-20-20_32.csv"):
+def setUpFiles(flights_path = "C:\\Users\\remem\\Downloads\\flights\\sorted_2018-01-20-20_32.csv", stop_at_line = 110646):
     readfile = open(flights_path, "r")
     writefiletraining = open("C:\\Users\\remem\\Downloads\\flights\\training.csv", 'w')
     writefiletests = open("C:\\Users\\remem\\Downloads\\flights\\tests.csv", 'w')
@@ -88,12 +14,14 @@ def setUpFiles(flights_path = "C:\\Users\\remem\\Downloads\\flights\\sorted_2018
             writefiletraining.write(line)
 
         lineNum = lineNum + 1
-        if (lineNum == 110646):  # 110646): #1047266*/):
+        if (lineNum == stop_at_line):  # 110646): #1047266*/):
             writefiletraining.close()
             writefiletests.close()
             break
 
-preprocess()
+    writefiletraining.close()
+    writefiletests.close()
+
 # num = 0
 # for line in (open("C:\\Users\\remem\\Downloads\\flights\\tests.csv", 'r')):
 #     num = num + 1
